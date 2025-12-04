1d25b5547749baedb3639cdf7ad2f5d751f8c17e
Increveloper <wklaver2010@gmail.com>
2025-12-04 14:12:47 +0100

    Added a helper for Bugfixing

--- GeneralFunctions.js
+++ GeneralFunctions.js
@@ -9,6 +9,20 @@ function formatNumber(num) {
     return `${cleanMantissa}e${exponent}`;
     // Done by ChatGPT
 }
+function formatNumberEdit(num) {
+    if (num < 1e6) return num.toFixed(2); // show normal up to 1M
+    console.log(num)
+
+    const exponent = Math.floor(Math.log10(num));
+    console.log(num)
+    const mantissa = num / Math.pow(10, exponent);
+    console.log(num)
+    // remove unnecessary decimals like 1.00 -> 1
+    const cleanMantissa = parseFloat(mantissa.toFixed(2)).toString();
+
+    return `${cleanMantissa}e${exponent}`;
+    // Done by ChatGPT
+}
 function logbase(value, base){
     return Math.log(value) / Math.log(base)
 }
